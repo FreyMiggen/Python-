1d665da76002dd3ab59a9961d40f9ce0eb3b6bce
FreyMiggen <70058907+FreyMiggen@users.noreply.github.com>
2022-06-26 22:10:36 +0700

    Update DFS-bài toán định  nghĩa.py

--- DFS-bài toán định  nghĩa.py
+++ DFS-bài toán định  nghĩa.py
@@ -1,10 +1,8 @@
-vertex=[(0,1),(1,2),(1,3),(1,4),(3,4)]
 graph=[[0 for i in range(5)] for j in range(5)]
 graph[0][1]=1
 graph[1][2]=1
 graph[1][3]=1
 graph[1][4]=1
-
 graph[3][4]=1
 for i in range(5):
     for j in range(5):
@@ -12,38 +10,22 @@ for i in range(5):
             graph[j][i]=1
 visited=[False for i in range(5)]
 track=[0]
+
+
 def DFS(v,stack):
     visited[v]=True
-    stack.append(v)
+    for i in range(5):
+        if graph[v][i]==1 and visited[i]==False:
+            visited[i]=True
+            track.append(i)
+            DFS(i,stack)
+            
+            stack.append(i)
+            
+            print(stack)
+            
+                
     
-    if len(stack)==0:
-        print("kim")
-        return True
-    else:
-        print('count',stack)
-        for i in range(5):
-            print(v,i)
-            if graph[v][i]==1 and visited[i]==False:
-                visited[i]=True
-        
-                track.append(i)
-                if DFS(i,stack):
-                    return True
-                print(stack,'before')
-                stack.pop()
-                
-                
-                
-                
-                print(stack)
-
-
-    return False
-
-
 DFS(0,[])
-print(track)
-print(visited)
-
     
     
